7c60b4b864e06562cae501f6ef79464c34e058d8
KonzernTechie <kv.nijesh@gmail.com>
2019-09-23 00:32:06 +0530

    Created an Absract Matrix class
    Signed-off-by: KonzernTechie <kv.nijesh@gmail.com>

---
+++ Mxgames/src/com/konzern/mxgames/AbstractMatrix.java
@@ -0,0 +1,16 @@
+/**
+ * AbstractMatrix.java
+ * 
+ * An abstract class 
+ */
+package com.konzern.mxgames;
+
+/**
+ * @author konzernites
+ * @since 1.0
+ *
+ */
+public abstract class AbstractMatrix<E> {
+	private static final long serialVersionUID = 8683452581122892183L;
+
+}
---
+++ Mxgames/src/com/konzern/mxgames/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author konzernites
+ * 
+ *
+ */
+package com.konzern.mxgames;
