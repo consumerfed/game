92d70ebbc583cb7bd5d4c031a8a3be7fd5464c9f
KonzernTechie <kv.nijesh@gmail.com>
2019-10-05 00:19:58 +0530

    instructions for tictactoe game added
    Signed-off-by: KonzernTechie <kv.nijesh@gmail.com>

--- Mxgames/src/com/konzern/mxgames/AbstractGame.java
+++ Mxgames/src/com/konzern/mxgames/AbstractGame.java
@@ -8,9 +8,17 @@ package com.konzern.mxgames;
  *
  */
 public abstract class AbstractGame {
-	
-	 public void playGame(IGame game) throws IllegalAccessException {
-		  game.startGame();
-		 }
-	 public abstract void instruction();
+
+	public void playGame(IGame game) throws IllegalAccessException {
+		game.startGame();
+	}
+
+	public abstract void instruction();
+
+	protected void footer() {
+		// TODO Auto-generated method stub
+		System.out.println(" ***** GAME OVER ***** ");
+		System.out.println(" Developed by consumerfed I T Section ");
+
+	}
 }
